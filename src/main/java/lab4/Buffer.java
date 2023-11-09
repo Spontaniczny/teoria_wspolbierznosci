6fe4a9e6bb04f2c715b25f32f6fea6e54acda5ea
Jakub Kasperski <k2002kuba@gmail.com>
2023-11-09 06:46:09 +0100

    lab4 ex1 done

--- src/main/java/lab4/Buffer.java
+++ src/main/java/lab4/Buffer.java
@@ -1,45 +1,26 @@
 package lab4;
 
 public class Buffer {
-
-    public String MyBuffer;
-    boolean is_empty = true;
-    public int production_size;
-//    private int no_
-    public int[] production;
+    public int[] productionLine;
+    public int numberOfProducers;
 
 
-    public Buffer(int production_size){
-        this.production_size = production_size;
-        this.production = new int[production_size];
+    public Buffer(int productionSize, int numberOfProducers){
+        this.numberOfProducers = numberOfProducers;
+        this.productionLine = new int[productionSize];
     }
-    synchronized public String take(int index){
-        while(is_empty){
+
+
+    public synchronized void Produce(int index, int ProducerID){
+        while(productionLine[index] != ProducerID){
             try {
                 wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        String m = this.MyBuffer;
-        this.MyBuffer = null;
-        is_empty = true;
-//        System.out.println("Consumer with ID " + ID + " took message: " + m);
-        notifyAll();
-        return m;
-    }
-
-    synchronized public void put(String s){
-        while(!is_empty){
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        this.MyBuffer = s;
-        is_empty = false;
-        System.out.println(s);
+        productionLine[index] = (ProducerID + 1) % numberOfProducers;
+        System.out.println("Produkcja na indexie " + index + " poszła do przodu na nr: " + productionLine[index]);
         notifyAll();
     }
 }
--- src/main/java/lab4/Producer.java
+++ src/main/java/lab4/Producer.java
@@ -2,21 +2,33 @@ package lab4;
 
 public class Producer implements Runnable {
     private Buffer buffer;
-    private int repeater;
-    private String myID;
-    private int index;
+    private int productionTime;
+    private int ID;
+    private int index = 0;
 
-    public Producer(Buffer buffer, int repeater, String ID) {
+    public Producer(Buffer buffer, int productionTime, int ID) {
         this.buffer = buffer;
-        this.repeater = repeater;
-        this.myID = ID;
+        this.productionTime = productionTime;
+        this.ID = ID;
     }
 
     public void run() {
 
-        for(int i = 0;  i < this.repeater;   i++) {
-            String m = "Producer with ID " + this.myID + " put message.";
-            buffer.put(m);
+        for(int i = 0;  i < buffer.productionLine.length * 5;  i++) {
+            try {
+                Produce();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    public void Produce() throws InterruptedException {
+//        while(buffer.productionLine[index] != ID){
+//            wait();
+//        }
+        Thread.sleep(productionTime);
+        buffer.Produce(index, ID);
+        index = (index + 1) % buffer.productionLine.length;
     }
 }
