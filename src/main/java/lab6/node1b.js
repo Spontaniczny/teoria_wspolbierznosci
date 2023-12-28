302b112273911b7f368ebafd358de313093abf09
Jakub Kasperski <k2002kuba@gmail.com>
2023-12-28 23:16:52 +0100

    lab 6 part 1 done

---
+++ src/main/java/lab6/node1b.js
@@ -0,0 +1,55 @@
+
+function printAsync(s, cb) {
+   var delay = Math.floor((Math.random()*1000)+500);
+   setTimeout(function() {
+       console.log(s);
+       if (cb) cb();
+   }, delay);
+}
+
+function task1(cb) {
+    printAsync("1", function() {
+        task2(cb);
+    });
+}
+
+function task2(cb) {
+    printAsync("2", function() {
+        task3(cb);
+    });
+}
+
+function task3(cb) {
+    printAsync("3", cb);
+}
+
+// wywolanie sekwencji zadan
+// task1(function() {
+//     console.log('done!');
+// });
+
+function loop(n) {
+    var delay = 5000;
+    for (let i = 0; i < n - 1; i++) {
+        // task1();
+        setTimeout(function() {
+            task1();
+        }, delay * i);
+      }
+
+    setTimeout(function() {
+        task1(function() {
+            console.log('done!');
+        });
+    }, delay * n);
+}
+
+
+/* 
+** Zadanie:
+** Napisz funkcje loop(n), ktora powoduje wykonanie powyzszej 
+** sekwencji zadan n razy. Czyli: 1 2 3 1 2 3 1 2 3 ... done
+** 
+*/
+
+loop(4);
