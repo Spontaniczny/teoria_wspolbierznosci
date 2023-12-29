db97eb82360917c3086a43fe0d8669859b60e3a1
Jakub Kasperski <k2002kuba@gmail.com>
2023-12-29 14:30:21 +0100

    Lab 6 exercise 1 done!

--- src/main/java/lab6/node1b.mjs
+++ src/main/java/lab6/node1b.mjs
@@ -1,3 +1,5 @@
+import waterfall from "async/waterfall.js";
+
 
 function printAsync(s, cb) {
    var delay = Math.floor((Math.random()*1000)+500);
@@ -28,22 +30,6 @@ function task3(cb) {
 //     console.log('done!');
 // });
 
-function loop(n) {
-    var delay = 5000;
-    for (let i = 0; i < n - 1; i++) {
-        // task1();
-        setTimeout(function() {
-            task1();
-        }, delay * i);
-      }
-
-    setTimeout(function() {
-        task1(function() {
-            console.log('done!');
-        });
-    }, delay * n);
-}
-
 
 /* 
 ** Zadanie:
@@ -52,4 +38,32 @@ function loop(n) {
 ** 
 */
 
-loop(4);
+// function loop1(n) {
+//     var delay = 5000;
+//     for (let i = 0; i < n - 1; i++) {
+//         // task1();
+//         setTimeout(function() {
+//             task1();
+//         }, delay * i);
+//       }
+
+//     setTimeout(function() {
+//         task1(function() {
+//             console.log('done!');
+//         });
+//     }, delay * n);
+// }
+
+function loop2(n) {
+    var tasks = [];
+    for (let i = 0; i < n; i++) {
+        tasks.push(task1);
+    }
+
+    waterfall(tasks);
+}
+
+
+
+// loop1(3);
+loop2(3);
